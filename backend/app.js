14cc5ab8d98b909fb9de40ac13cd3bd90851b8c5
dangtranchinh <dangtranchinh1107@gmail.com>
2024-01-19 00:22:02 +0700

    setup backend

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+const { PORT, mongoDB } = process.env;
+
+import productRoutes from "./routes/products.js";
+
+app.use("/api/v1", productRoutes);
+
+// Kết nối Db
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${mongoDB}`);
+    console.log("Mongodb connencted ");
+    app.listen(PORT, () => {
+      console.log(`Back-end is running ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectDB();
