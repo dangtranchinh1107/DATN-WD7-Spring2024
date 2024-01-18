a465bc14c6bbf045bee7e019fd03afae721ef3c6
dangtranchinh <dangtranchinh1107@gmail.com>
2024-01-19 01:49:54 +0700

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "./index.css";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1 className="text-3xl font-bold underline ">Hello TechLap!</h1>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
