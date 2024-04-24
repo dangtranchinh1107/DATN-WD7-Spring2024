7f0aec5f4099832a37b85847eb4c26f3fd864d90
tamle26 <tamldph29582@fpt.edu.vn>
2024-04-24 22:57:34 +0700

    updatgiaodien

---
+++ frontend/src/components/cart/Cart.jsx
@@ -0,0 +1,167 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import MetaData from "../layout/MetaData";
+import { setCartItem, removeCartItem } from "../../redux/features/cartSlice";
+
+const Cart = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const increaseQty = (item) => {
+    const newQty = item.quantity + 1;
+    if (newQty > item.stock) return;
+    dispatch(setCartItem({ ...item, quantity: newQty }));
+  };
+
+  const decreaseQty = (item) => {
+    const newQty = item.quantity - 1;
+    if (newQty <= 0) return;
+    dispatch(setCartItem({ ...item, quantity: newQty }));
+  };
+
+  const removeCartItemHandler = (id) => {
+    if (window.confirm("Bạn có chắc chắn muốn xóa?")) {
+      dispatch(removeCartItem(id));
+    }
+  };
+
+  const checkoutHandler = () => {
+    navigate("/shipping");
+  };
+
+  return (
+    <>
+      <MetaData title={"Giỏ hàng của bạn"} />
+      {cartItems.length === 0 ? (
+        <div>
+          <img
+            className="w-50 d-flex"
+            src="https://fptshop.com.vn/estore-images/empty-cart.png"
+            alt=""
+          />
+          <h2 className="text-center text-danger fw-bold">
+            Chưa có sản phẩm nào trong giỏ hàng
+          </h2>
+          <div className="d-flex">
+            <Link className="btn btn-warning text-white " to={"/"}>
+              Mua ngay
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <>
+          <h2 className="mt-5 fs-4">
+            Giỏ hàng của bạn : <b>{cartItems.length} sản phẩm</b>
+          </h2>
+          <div className="d-flex justify-between w-full gap-[120px] ">
+            <div className="col-12 col-lg-8">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Hình ảnh</th>
+                    <th scope="col">Tên sản phẩm</th>
+                    <th scope="col">Giá tiền</th>
+                    <th scope="col">Số lượng</th>
+                    <th scope="col">Tổng tiền</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cartItems.map((item) => (
+                    <tr key={item.product}>
+                      <td>
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                          height="90"
+                          width="115"
+                        />
+                      </td>
+                      <td>
+                        <Link to={`/products/${item.product}`}>
+                          {item.name}
+                        </Link>
+                      </td>
+                      <td>${item.price}</td>
+                      <td>
+                        <div className="stockCounter">
+                          <span
+                            className="btn btn-danger minus"
+                            onClick={() => decreaseQty(item)}
+                          >
+                            -
+                          </span>
+                          <input
+                            type="number"
+                            className="form-control count d-inline"
+                            value={item.quantity}
+                            readOnly
+                          />
+                          <span
+                            className="btn btn-primary plus"
+                            onClick={() => increaseQty(item)}
+                          >
+                            +
+                          </span>
+                        </div>
+                      </td>
+                      <td>${item.price * item.quantity}</td>
+                      <td>
+                        <i
+                          id="delete_cart_item"
+                          className="fa fa-trash btn btn-danger"
+                          onClick={() => removeCartItemHandler(item.product)}
+                        ></i>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="w-1/2 flex justify-end ">
+              <div className="px-4 py-8 rounded border border-gray-400 w-full max-w-[470px] p-5 pe-5 ps-5 shadow mb-5 bg-body-tertiary ">
+                <h2 className="text-xl font-normal mb-6">Tổng thanh toán</h2>
+                <div className="flex justify-between items-center mb-4 pb-4 border-b border-b-gray-400">
+                  <p className="text-base">
+                    Số lượng:{" "}
+                    <span className="font-bold text-base fw-bolder">
+                      {cartItems.reduce((acc, item) => acc + item.quantity, 0)}{" "}
+                    </span>
+                  </p>
+                </div>
+
+                <div className="flex justify-between items-center mb-4 pb-4">
+                  <p className="text-base ">
+                    Tổng cộng:{" "}
+                    <span className="text-base font-bold fw-bolder">
+                      $
+                      {cartItems
+                        .reduce(
+                          (acc, item) => acc + item.quantity * item.price,
+                          0
+                        )
+                        .toFixed(2)}
+                    </span>
+                  </p>
+                </div>
+
+                <button
+                  id="checkout_btn"
+                  className="btn bg-danger text-white w-100 font-medium text-base py-4 px-12 rounded w-fit mx-auto"
+                  onClick={checkoutHandler}
+                >
+                  Thanh toán
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Cart;
---
+++ frontend/src/components/product/ProductDetails.jsx
@@ -0,0 +1,203 @@
+import React, { useEffect, useState } from "react";
+import { useGetProductDetailsQuery } from "../../redux/api/productsApi";
+import { useParams } from "react-router-dom";
+import Loader from "../layout/Loader";
+import toast from "react-hot-toast";
+import StarRatings from "react-star-ratings";
+import { useDispatch, useSelector } from "react-redux";
+import { setCartItem } from "../../redux/features/cartSlice";
+import NewReviews from "../reviews/NewReviews";
+import ListReviews from "../reviews/ListReviews";
+import Magnifier from "react-magnifier";
+
+const ProductDetails = () => {
+  const params = useParams();
+  const dispatch = useDispatch();
+  const [quantity, setQuantity] = useState(1);
+
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  const { data, isLoading, error, isError } = useGetProductDetailsQuery(
+    params?.id
+  );
+  const product = data?.product;
+  // console.log(product);
+
+  const [activeImg, setActiveImg] = useState("");
+
+  useEffect(() => {
+    setActiveImg(
+      product?.images[0]
+        ? product?.images[0]?.url
+        : "/images/default_product.png"
+    );
+  }, [product]);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(error?.data?.message);
+    }
+  }, [isError]);
+
+  const increseQty = () => {
+    const count = document.querySelector(".count");
+    if (count.valueAsNumber >= product?.stock) return;
+    const qty = count.valueAsNumber + 1;
+    setQuantity(qty);
+  };
+  const decreseQty = () => {
+    const count = document.querySelector(".count");
+    if (count.valueAsNumber <= 1) return;
+    const qty = count.valueAsNumber - 1;
+    setQuantity(qty);
+  };
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value);
+    if (!isNaN(value) && value >= 1 && value <= product?.stock) {
+      setQuantity(value);
+    }
+  };
+  const setItemToCart = () => {
+    const cartItem = {
+      product: product?._id,
+      name: product?.name,
+      price: product?.price,
+      image: product?.images[0]?.url,
+      stock: product?.stock,
+      quantity,
+    };
+
+    dispatch(setCartItem(cartItem));
+    toast.success("Thêm vào giỏ hàng thành công!");
+  };
+  if (isLoading) return <Loader />;
+  return (
+    <>
+      <div className="row d-flex justify-content-around">
+        <div className="col-12 col-lg-5 img-fluid" id="product_image">
+          <div className="p-3">
+            <Magnifier src={activeImg} alt={product?.name} width={500} />;
+            {/* <img
+              className="d-block w-100"
+              src={activeImg}
+              alt={product?.name}
+              width="340"
+              height="390"
+            /> */}
+          </div>
+          <div className="row justify-content-start mt-5">
+            {product?.images?.map((img) => (
+              <div className="col-2 ms-4 mt-2">
+                <a role="button">
+                  <img
+                    className={`d-block border rounded p-3 cursor-pointer ${
+                      img.url === activeImg ? "border-warning" : ""
+                    } `}
+                    height="100"
+                    width="100"
+                    src={img?.url}
+                    alt={img?.url}
+                    onClick={(e) => setActiveImg(img.url)}
+                  />
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="col-12 col-lg-5 mt-5">
+          <h3>{product?.name}</h3>
+          <p id="product_id">Sản phẩm # {product?._id}</p>
+
+          <hr />
+
+          <div className="">
+            <div className="star-ratings  mb-3">
+              <StarRatings
+                rating={product?.ratings}
+                starRatedColor="#ffb829"
+                numberOfStars={5}
+                name="rating"
+                starDimension="24px"
+                starSpacing="1px"
+              />
+            </div>
+            <span id="no-of-reviews" className="pt-1 ps-2">
+              {" "}
+              ({product?.numOfReviews} Đánh giá ){" "}
+            </span>
+          </div>
+          <hr />
+
+          <p id="product_price">${product?.price}</p>
+          <div className="stockCounter d-inline">
+            <span className="btn btn-danger minus" onClick={decreseQty}>
+              -
+            </span>
+            <input
+              type="number"
+              className="form-control count d-inline"
+              value={quantity}
+              onChange={handleChange}
+            />
+            <span className="btn btn-primary plus" onClick={increseQty}>
+              +
+            </span>
+          </div>
+          <button
+            type="button"
+            id="cart_btn"
+            className="btn btn-primary bg-warning rounded-2 text-white d-inline ms-4"
+            disabled={product.stock <= 0}
+            onClick={setItemToCart}
+          >
+            Thêm vào giỏ hàng
+          </button>
+
+          <hr />
+
+          <p>
+            Tình trạng:{" "}
+            <span
+              id="stock_status"
+              className={product?.stock > 0 ? "greenColor" : "redColor"}
+            >
+              {product?.stock > 0 ? "Còn hàng" : "Hết hàng"}
+            </span>
+            ({product?.stock})
+          </p>
+
+          <hr />
+
+          <h4 className="mt-2">Mô tả:</h4>
+
+          <p>{product?.description}</p>
+
+          <p>Màu sắc: {product?.color[0]?.name}</p>
+          <p>Hãng sản xuất: {product?.category[0]?.name}</p>
+          <p>CPU: {product?.cpu[0]?.type}</p>
+          <p>Card đồ họa: {product?.graphicCard[0]?.type}</p>
+          <p>Ổ cứng: {product?.hardDisk[0]?.type}</p>
+          <p>Ram: {product?.ram[0]?.type}</p>
+          <hr />
+          <p id="product_seller mb-3">
+            <p>Trạng thái: {product?.status}</p>
+          </p>
+
+          {isAuthenticated ? (
+            <NewReviews productId={product?._id} />
+          ) : (
+            <div className="alert alert-danger my-5" type="alert">
+              Đăng nhập để gửi đánh giá của bạn.
+            </div>
+          )}
+        </div>
+      </div>
+      {product?.reviews?.length > 0 && (
+        <ListReviews reviews={product?.reviews} />
+      )}
+    </>
+  );
+};
+
+export default ProductDetails;
